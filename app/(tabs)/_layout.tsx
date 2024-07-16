63343bd19aa5c8acbe0cdd2613c32904d35bd3c2
Thiago Pires <thiagopac@gmail.com>
2024-07-16 15:58:52 -0300

    Criação de projeto e tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Link, Tabs } from "expo-router";
+import { Pressable, Image } from "react-native";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome6>["name"];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome6 size={28} style={{ marginBottom: -3 }} {...props} />;
 }
+
+const NavBarTitle = () => (
+  <Image
+    source={require("@/assets/images/logo-color.png")}
+    style={{ width: 100, height: 40 }}
+    resizeMode="contain"
+  />
+);
+
+const MenuButton = () => (
+  <Link href="/modal" asChild>
+    <Pressable>
+      {({ pressed }) => (
+        <FontAwesome
+          name="bars"
+          size={25}
+          // color={Colors[colorScheme ?? "light"].text}
+          color={Colors["light"].text}
+          style={{ marginLeft: 15, opacity: pressed ? 0.5 : 1 }}
+        />
+      )}
+    </Pressable>
+  </Link>
+);
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -21,37 +45,38 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: Colors["light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+        headerTitle: () => <NavBarTitle />,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: "Loja",
+          tabBarIcon: ({ color }) => <TabBarIcon name="store" color={color} />,
+          headerLeft: MenuButton,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="rebuy"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Recompra",
+          tabBarIcon: ({ color }) => <TabBarIcon name="repeat" color={color} />,
+          headerLeft: MenuButton,
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Sacola",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="bag-shopping" color={color} />
+          ),
+          headerLeft: MenuButton,
         }}
       />
     </Tabs>
--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -3,16 +3,16 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabOneScreen() {
+export default function TabCartScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Loja</Text>
+      <Text style={styles.title}>Sacola</Text>
       {/* <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
-      /> */}
-      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+      />
+      <EditScreenInfo path="app/(tabs)/cart.tsx" /> */}
     </View>
   );
 }
